e0978a8509fa30d4c2c7b74c19bbf0b384329096
AllenSun7 <allensun623@gmail.com>
2021-03-27 10:28:07 -0500

    tweet info format

--- tweets_collection/README.md
+++ tweets_collection/README.md
@@ -2,3 +2,19 @@
 
 ## Dataset
 [HSpam14 Dataset](https://www3.ntu.edu.sg/home/AXSun/datasets.html)
+
+## Feature Set
+| Feature | Description |
+| --- | --- |
+| tweet | This is the text that was tweeted |
+| no_followers | The number of followers of this twitter user |
+| no_followings | The number of followings/friends of this twitter user |
+| no_userfavourites | The number of favorites this twitter user received |
+| no_lists | The number of lists this twitter user added |
+| no_tweets | The number of tweets this twitter user sent |
+| no_favorites | The number of favorites this tweet |
+| no_retweets | The number of retweets this tweet |
+| no_hashtags | The number of hashtags included in this tweet |
+| no_usermentions | The number of user mentions included in this tweet |
+| no_urls | The number of URLs included in this tweet |
+
--- tweets_collection/main.py
+++ tweets_collection/main.py
@@ -2,11 +2,28 @@ from api_auth import api_auth
 # Twitter Developer keys here
 # It is CENSORED
 
+
+
+
 def run(api, tweet_ids):
     for tweet_id in tweet_ids:
+        print("-" * 50)
         try:
             tweet = api.get_status(tweet_id)
-            print(tweet.text)
+            tweet_feature = {
+                "tweet": tweet.text,
+                "no_followers": tweet.user.followers_count,
+                "no_followings": tweet.user.friends_count,
+                "no_userfavorites": tweet.user.favourites_count,
+                "no_lists": tweet.user.listed_count,
+                "no_tweets": tweet.user.statuses_count,
+                "no_retweets": tweet.retweet_count,
+                "no_favorites": tweet.favorite_count,
+                "no_hashtags": len(tweet.entities["hashtags"]),
+                "no_usermentions": len(tweet.entities["user_mentions"]),
+                "no_urls": len(tweet.entities["urls"]),
+            }
+            print(tweet_feature)
         except Exception as e:
             print(e)
 
